6106f22932e072803803cee419eb4125050355d2
Meggan M Green <github@meggangreen.com>
2018-01-06 00:44:51 +0000

    psuedo code

--- 02-1.py
+++ 02-1.py
@@ -1,1 +1,15 @@
-02-1.py
+""" Day 2 challenge 1: determine the checksum.
+
+    Given a spreadsheet of values, subtract the smallest from the largest of
+    each row, then add differences. The puzzle input is in 02-1.txt.
+
+"""
+
+
+    # checksum = 0
+    # with open file
+    # for each line
+    #   find the min and max
+    #   subtract
+    #   add difference to checksum
+    # return checksum

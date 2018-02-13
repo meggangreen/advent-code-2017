0d2cb648977f741a0e4ceb48b3ee756b3c7e7048
Meggan M Green <github@meggangreen.com>
2018-02-13 01:53:53 +0000

    first run

---
+++ 06.py
@@ -0,0 +1,20 @@
+def count_redistribution_cycles():
+    # vars: count, len, configs, curr_config
+
+    file = open('06.txt').read()
+    banks = [int(number) for number in file.split(',')]
+
+    configs = set()
+    curr_config = banks
+
+    while curr_config not in configs:
+        configs.add(curr_config)
+        max_blocks = max(curr_config)
+        i = index(max(curr_config))
+
+        while max_blocks > 0:
+            if i + 1 == len(curr_config):
+                i = -1
+            i += 1
+            curr_config[i] += 1
+            max_blocks += -1

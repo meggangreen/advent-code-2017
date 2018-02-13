5819186e7844e3307249f5210fd020814b00d326
Meggan M Green <github@meggangreen.com>
2018-02-13 02:21:47 +0000

    run 3

--- 06.py
+++ 06.py
@@ -6,19 +6,25 @@ def count_redistribution_cycles():
 
     configs = set()
     curr_config = banks
-    count = -1
+    curr_config_s = ",".join([str(number) for number in curr_config])
 
-    while curr_config not in configs:
-        count += 1
-        configs.add(",".join([str(number) for number in curr_config]))
+    while curr_config_s not in configs:
+        configs.add(curr_config_s)
         max_blocks = max(curr_config)
-        i = index(max(curr_config))
+        i = curr_config.index(max(curr_config))
+        curr_config[i] = 0
+
+        import pdb; pdb.set_trace()
+        print curr_config_s, len(configs), max_blocks
 
         while max_blocks > 0:
             if i + 1 == len(curr_config):
                 i = -1
             i += 1
-            curr_config[i] += 1
+            val = curr_config[i]
+            curr_config[i] = val + 1
             max_blocks += -1
 
-    return count
+        curr_config_s = ",".join([str(number) for number in curr_config])
+
+    return len(configs)

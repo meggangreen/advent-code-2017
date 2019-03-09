74e889800fe6b950855f8ba717c8538cc65b6f55
Meggan M Green <github@meggangreen.com>
2019-03-09 00:16:44 +0000

    part 2 done

--- files/11.py
+++ files/11.py
@@ -1,9 +1,13 @@
-def part_one(filepath):
+from copy import deepcopy
+
+def find_child_distance(filepath):
     with open(filepath) as file:
         path = file.read().split(',')
 
     nodes = {'nw': 0, 'n': 0, 'ne': 0, 'sw': 0, 's': 0, 'se': 0}
     opps = {'nw': 'se', 'n': 's', 'ne': 'sw', 'sw': 'ne', 's': 'n', 'se': 'nw'}
+
+    max_away = 0
 
     for node in path:
         if nodes[opps[node]] > 0:
@@ -11,30 +15,52 @@ def part_one(filepath):
         else:
             nodes[node] += 1
 
+        max_away = max(max_away, sum(shorten_path(deepcopy(nodes)).values()))
+
+    return sum(shorten_path(nodes).values()), max_away
+
+
+def shorten_path(nodes):
+
     if nodes['nw'] and nodes['s']:
         while nodes['nw'] > 0 and nodes['s'] > 0:
             nodes['sw'] += 1
             nodes['s'] += -1
             nodes['nw'] += -1
+
     if nodes['ne'] and nodes['s']:
         while nodes['ne'] > 0 and nodes['s'] > 0:
             nodes['se'] += 1
             nodes['s'] += -1
             nodes['ne'] += -1
+
     if nodes['sw'] and nodes['n']:
         while nodes['sw'] > 0 and nodes['n'] > 0:
             nodes['nw'] += 1
             nodes['n'] += -1
             nodes['sw'] += -1
+
     if nodes['se'] and nodes['n']:
         while nodes['se'] > 0 and nodes['n'] > 0:
             nodes['ne'] += 1
             nodes['n'] += -1
             nodes['se'] += -1
 
-    return sum(nodes.values())
+    if nodes['ne'] and nodes['nw']:
+        while nodes['ne'] > 0 and nodes['nw'] > 0:
+            nodes['n'] += 1
+            nodes['nw'] += -1
+            nodes['ne'] += -1
+
+    if nodes['se'] and nodes['sw']:
+        while nodes['se'] > 0 and nodes['sw'] > 0:
+            nodes['s'] += 1
+            nodes['sw'] += -1
+            nodes['se'] += -1
+
+    return nodes
 
 
 ################################################################################
 if __name__ == '__main__':
-    print(f"Part 1: {part_one('11.txt')}")
+    print(f"Parts 1, 2: {find_child_distance('11.txt')}")
